4f3c411cbe1cac010b3cc1a1179b1413f81e06bb
Sagar <sgavhane70@gmail.com>
2022-08-13 21:19:16 +0530

    feat: add cart app

---
+++ container/src/bootstrap.js
@@ -0,0 +1,5 @@
+import { mount as mount1 } from "products/ProductsIndex";
+import { mount as mount2 } from "cart/CartShow";
+
+mount1(document.querySelector("#dev-products"));
+mount2(document.querySelector("#dev-cart"));
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,18 @@ module.exports = {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/bootstrap",
+      },
+      shared: {
+        faker: {
+          singleton: true,
+        },
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
